3d927af8736b068cf4b52a5a02c345fd0bc70bc7
Rodrigo <rhefew@gmail.com>
2021-03-22 13:41:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const mongoose = require('mongoose')
 const app = express()
 
 
-const mongoDB = "mongodb+srv://rhefew:jTiZGkJFITeGcGyA@tc-achievements-api-clu.uqpj4.mongodb.net/tc-labdays-achievements?retryWrites=true&w=majority&readPreference=secondary"
-// const mongoDB = "mongodb://localhost:27017/matches?readPreference=primary&appname=MongoDB%20Compass&ssl=false&retryWrites=true&w=majority"
+const mongoDB = "mongodb://localhost:27017/matches?readPreference=primary&appname=MongoDB%20Compass&ssl=false&retryWrites=true&w=majority"
 mongoose.connect(mongoDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
